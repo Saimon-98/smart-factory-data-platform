19f8d21161bcd7c906187bf4e6c6b632792e3c9d
Saimon-98 <simonelm8991@hotmail.com>
2026-01-20 19:11:48 +0100

    Add logging for ETL process simulating CloudWatch

--- etl/main.py
+++ etl/main.py
@@ -2,8 +2,17 @@ import psycopg2
 from extract import extract_data
 from transform import transform_data
 from load import load_data
+import logging
+
+# Configurazione logging
+logging.basicConfig(
+    filename="logs/etl.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 def main():
+    logging.info("ETL started")
     # Connessione DB
     conn = psycopg2.connect(
         host="localhost",
@@ -16,23 +25,28 @@ def main():
     try:
         # Extract
         rows = extract_data(conn)
-        print(f"Extracted {len(rows)} rows")
+        logging.info(f"Extracted {len(rows)} rows")
 
         # Transform
         transformed, rejected = transform_data(rows)
-        print(f"Transformed into {len(transformed)} aggregated rows")
-        print(f"Rejected rows: {len(rejected)}")
+        logging.info(f"Transformed into {len(transformed)} aggregated rows")
+        logging.info(f"Rejected rows: {len(rejected)}")
 
         # (opzionale) Mostra esempi di scarti (i primi 5 elementi)
         for r, reason in rejected[:5]:
-            print(f"Rejected {r} → {reason}")
+            logging.warning(f"Rejected {r} → {reason}")
 
         # Load
         load_data(conn, transformed)
-        print("Load completed successfully")
+        logging.info("Load completed successfully")
+
+    except Exception as e:
+        logging.error(f"ETL failed: {e}")
+        raise
 
     finally:
         conn.close()
+        logging.info("ETL finished\n")
 
 if __name__ == "__main__":
     main()
